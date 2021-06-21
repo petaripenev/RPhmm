338646ff7e1e196df837cf3dfc9d8280c442a0e2
Petar Penev <peteripenev@gmail.com>
2021-06-21 15:14:36 -0700

    run Hmm commands

---
+++ runHMM.sh
@@ -0,0 +1,3 @@
+for f in RP_HMMs/2/*; do g=${f##*/}; hmmsearch --tblout ./hmmOUT/Bacteria/${g%.hmm}_RifSed_csp1_19ft_2_Methanoperedens_44_10.out --cpu 12 $f ./proteinSeqs/RifSed_csp1_19ft_2_Methanoperedens_44_10.proteins.faa ;done
+
+for f in RP_HMMs/2157/*; do g=${f##*/}; hmmsearch --tblout ./hmmOUT/Archaea/${g%.hmm}_RifSed_csp1_19ft_2_Methanoperedens_44_10.out --cpu 12 $f ./proteinSeqs/RifSed_csp1_19ft_2_Methanoperedens_44_10.proteins.faa ;done
